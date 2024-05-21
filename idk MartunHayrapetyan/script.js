566f218294aa5c90acd9876b044650e6934bbaad
MartunHayrapetyan <martun.h@tumo.org>
2024-05-21 17:15:01 +0400

    new buttons and functional

--- idk MartunHayrapetyan/script.js
+++ idk MartunHayrapetyan/script.js
@@ -1,29 +1,29 @@
 let data = {
     title: [
-    "Твоя любовь манила", 
-    "Далеко", 
-    "Stereo Love x On The Floor", 
-    "WTF 2", 
-    "AHappy Nation"],
+        "Твоя любовь манила",
+        "Далеко",
+        "Stereo Love x On The Floor",
+        "WTF 2",
+        "AHappy Nation"],
     artist: [
-    "Тахмина",
-    "BUSHIDO ZHO",
-    "Unknown",
-    "Ugovhb",
-    "Ace Of Base"
+        "Тахмина",
+        "BUSHIDO ZHO",
+        "Unknown",
+        "Ugovhb",
+        "Ace Of Base"
     ],
     song: [
-    "music/TvoyaLyubovManila.mp3", 
-    "music/Daleko.mp3", 
-    "music/OnTheFLoorStereoLove.mp3", 
-    "music/UgovhbWTF2.mp3", 
-    "music/AOBHappyNationS&R.mp3"],
+        "music/TvoyaLyubovManila.mp3",
+        "music/Daleko.mp3",
+        "music/OnTheFLoorStereoLove.mp3",
+        "music/UgovhbWTF2.mp3",
+        "music/AOBHappyNationS&R.mp3"],
     poster: [
-    "https://www.domkrat.by/img/2021/audi/audi_rs7_minsk_01.jpg", 
-    "https://images.genius.com/76fdf4f8e43e177a400ee1a8b212ce20.1000x1000x1.jpg", 
-    "https://i.ytimg.com/vi/83r7aFelYn4/maxresdefault.jpg", 
-    "https://i.scdn.co/image/ab67616d00001e0229a6f1ee44ca818bffddc870", 
-    "https://i.ytimg.com/vi/G_ukZsiplG0/maxresdefault.jpg"]
+        "https://www.domkrat.by/img/2021/audi/audi_rs7_minsk_01.jpg",
+        "https://images.genius.com/76fdf4f8e43e177a400ee1a8b212ce20.1000x1000x1.jpg",
+        "https://i.ytimg.com/vi/83r7aFelYn4/maxresdefault.jpg",
+        "https://i.scdn.co/image/ab67616d00001e0229a6f1ee44ca818bffddc870",
+        "https://i.ytimg.com/vi/G_ukZsiplG0/maxresdefault.jpg"]
 }
 
 
@@ -57,10 +57,8 @@ function playOrPause() {
     }
 }
 song.addEventListener("timeupdate", function () {
-    let fill = document.getElementsByClassName("fill")
     let fill2 = document.getElementsByClassName("fill2")
     let position = song.currentTime / song.duration
-    fill[0].style.marginLeft = (position * 100 - 1)+ "%"
     fill2[0].style.width = position * 100 + "%"
     convertTime(song.currentTime)
     totalTime(song.duration)
@@ -100,30 +98,30 @@ function prev() {
 }
 
 
-function volumeRange(){
+function volumeRange() {
     let volumeImg = document.getElementById("volume");
     let volumeImgMute = document.getElementById("volume-mute");
     let volumeImgDown = document.getElementById("volume-down");
     let volumeImgMoreDown = document.getElementById("volume-down+");
     let range = document.getElementById("range");
     song.volume = range.value / 100
-    
-    if(range.value == 0){
+
+    if (range.value == 0) {
         volumeImgMute.style.opacity = 100
         volumeImg.style.opacity = 0
         volumeImgDown.style.opacity = 0
         volumeImgMoreDown.style.opacity = 0
-    } else if(range.value > 55){
+    } else if (range.value > 55) {
         volumeImgMute.style.opacity = 0
         volumeImg.style.opacity = 100
         volumeImgDown.style.opacity = 0
         volumeImgMoreDown.style.opacity = 0
-    }else if(range.value < 25){
+    } else if (range.value < 25) {
         volumeImgMute.style.opacity = 0
         volumeImg.style.opacity = 0
         volumeImgDown.style.opacity = 0
         volumeImgMoreDown.style.opacity = 100
-    }else{
+    } else {
         volumeImgMute.style.opacity = 0
         volumeImg.style.opacity = 0
         volumeImgDown.style.opacity = 100
@@ -134,7 +132,7 @@ function volumeRange(){
 let vlmButtons = document.getElementsByClassName("mute");
 let range = document.getElementById("range");
 
-vlmButtons[0].addEventListener("click", function() {
+vlmButtons[0].addEventListener("click", function () {
     if (range.classList.contains("visible")) {
         range.classList.remove("visible");
     } else {
@@ -142,5 +140,33 @@ vlmButtons[0].addEventListener("click", function() {
     }
 });
 
+let handle = document.getElementById('handle');
 
-//test
+handle.addEventListener('click', function (event) {
+    let rect = handle.getBoundingClientRect();
+    let offsetX = event.clientX - rect.left;
+    let handleWidth = handle.clientWidth;
+    let newPosition = offsetX / handleWidth;
+    song.currentTime = newPosition * song.duration;
+});
+
+let forward10 = document.getElementById("forward")
+let backward10 = document.getElementById("backward")
+
+
+forward10.addEventListener('click', function(){
+    if(song.currentTime < song.duration - 10){
+        song.currentTime = song.currentTime + 10
+    }
+    else{
+        song.currentTime = song.duration - 1
+    }
+})
+backward10.addEventListener('click', function(){
+    if(song.currentTime > 10){
+        song.currentTime = song.currentTime - 10
+    }
+    else{
+        song.currentTime = 0
+    }
+})
